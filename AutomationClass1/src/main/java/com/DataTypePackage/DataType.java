0f033b369a194e052de5a398af0920f80801d638
Administrator <Administrator@192.168.86.42>
2020-02-02 16:47:19 -0500

    1st push

---
+++ AutomationClass1/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,20 @@
+package com.DataTypePackage;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x =1;
+		int t = 2;
+		double y = 2.5;
+		char z = 'A';
+		String name = "Steve";	// Non primitive data type must start with capital
+		boolean b = false;		// true
+		
+		System.out.println("Hello World");
+				
+		
+	}
+
+}
